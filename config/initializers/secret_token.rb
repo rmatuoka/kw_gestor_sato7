e9073d9c83b01b292c3af4045243cfc32084986d
Andre <aventura@sato7.com.br>
2014-09-24 16:31:32 -0300

    Inicio do Projeto

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KwGestorSato7::Application.config.secret_token = '7d2fcb925c3219edf2ba68d47bafd717d55ed790837bd7d79595acc96a640c4509dcb09272f0dddafa5f10b7d2de8f07107e1f2ea0203062d8c94d1f21be6343'
